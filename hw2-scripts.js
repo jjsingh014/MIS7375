82bb617290e38db2bed55efae7b86d428c2b68a9
jjsingh014 <jassinavi.singh02@gmail.com>
2024-10-17 19:03:51 -0500

    Update hw2-scripts.js

--- hw2-scripts.js
+++ hw2-scripts.js
@@ -24,12 +24,11 @@ function getData(){
     form_data_output="<table class='useroutput'><th>Name</th><<th>Value</th>";
     for (i = 0; i < form_data.length; i++) {
          console.log("item: "+i+" "+form_data.elements[i].name+" = "+form_data.elements[i].value);
-         data = form_data.elements[i].for;
+         data = form_data.elements[i].type;
          switch (data){
           case "radio":
                 if (form_data.elements[i].checked){
                     form_data_output = form_data_output + "<tr><td align='right'>"+form_data.elements[i].name+"</td>";
-                    form_data_output = form_data_output +"<td align='right'>"+ data + "</td>";
                     form_data_output = form_data_output +"<td class='outputdata'>"+ form_data.elements[i].value+"</td></tr>";
                 }
            break;
