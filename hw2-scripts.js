cce44bbcdd024dcf80c60eeccdc3ca31699c50fe
jjsingh014 <jassinavi.singh02@gmail.com>
2024-10-17 20:54:14 -0500

    Update hw2-scripts.js

--- hw2-scripts.js
+++ hw2-scripts.js
@@ -118,7 +118,7 @@ function pwdcheck() {
 function checkDoB(){
    var doc = document;
    var birth = doc.getElementById("dob");
-   var checkValue = doc.getElementById("checkValue");
+   var checkValue = doc.getElementById("getData1");
    
    checkValue.addEventListener("click", function(){
    var birthDate = new Date(birth.value);
