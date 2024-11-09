09ff126d0cb589264aaea4d850c3e24dfb76bdad
jjsingh014 <jassinavi.singh02@gmail.com>
2024-11-08 23:50:51 -0600

    Update hw3-scripts.js

--- hw3-scripts.js
+++ hw3-scripts.js
@@ -196,16 +196,11 @@ function phonevalidation(){
         }
     }
  
-   if (phoneinput.search(/[-]/)< 0){
+   if (phoneinput.search(/[-]/)< 0 && phoneinput.search(/[-]/)= 2{
        phoneoutput = "The phone number has no dashes.";
        error_flag = 1;
    }else {
-        if (phoneinput.search(/[-]/=2) {
-           phoneoutput = "You need 2 dashes";
-           error_flag = 1;
-       }else  {
-          phoneoutput = "It has 2 dashes.";
-        }
+       phoneoutput = "It has dashes";
    }
    document.getElementById("phone_message2").innerHTML = phoneoutput;
 }
