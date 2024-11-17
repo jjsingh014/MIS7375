28ca43d8cd0f7a2540e8a01a54b88a09dc2a99d7
jjsingh014 <jassinavi.singh02@gmail.com>
2024-11-17 14:46:46 -0600

    Update hw4-scripts.js

--- HW4/hw4-scripts.js
+++ HW4/hw4-scripts.js
@@ -235,3 +235,37 @@ function useridvalidation(){
         }
     }
    }
+
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let ca = document.cookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function checkCookie() {
+  let user = getCookie("fname");
+  if (user != "") {
+    alert("Welcome again " + user);
+  } else {
+    user = alert("Welcome new user! Please fill the form below.");
+    if (user != "" && user != null) {
+      setCookie("username", user, 2);
+    }
+  }
+}
